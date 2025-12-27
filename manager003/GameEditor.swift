f9f512befab000e4af9292e5d215778695e40ea7
bernard milman <milmedsoft@gmail.com>
2025-12-27 12:23:00 -0500

    lesson 11

---
+++ manager003/GameEditor.swift
@@ -0,0 +1,18 @@
+//
+//  GameEditor.swift
+//  manager003
+//
+//  Created by bernard milman on 12/27/25.
+//
+
+import SwiftUI
+
+struct GameEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameEditor()
+}
