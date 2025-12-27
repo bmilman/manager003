caec713a21472725302220a28db1c387e5125da8
bernard milman <gkjhkj@yhioyhik.com>
2025-12-27 18:15:15 -0500

    lesson 12 beginning, first 20 min

---
+++ manager003/PegChoicesChooser.swift
@@ -0,0 +1,18 @@
+//
+//  PegChoicesChooser.swift
+//  manager003
+//
+//  Created by bernard milman on 12/27/25.
+//
+
+import SwiftUI
+
+struct PegChoicesChooser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PegChoicesChooser()
+}
